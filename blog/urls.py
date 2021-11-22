f351f95550bb4ce80b626bff683b8575f6a7a97c
xander <karimi.xander@gmail.com>
2021-11-22 19:55:44 +0330

    about route

--- blog/urls.py
+++ blog/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
 
-import blog.views
+app_name='blog'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("",blog.views.home,name="home"),
-    path('blog/',include('blog.urls'))
+    path("about/",views.about,name="abut"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render,HttpResponse
 
 def home(request):
     return render(request,'home.html',{"data":"i'm xander"})
+def about(request):
+    return render(request,'about.html')
